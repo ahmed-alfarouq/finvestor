2feb211080d50c36e28a3ccbb7e1392089660548
ahmed-al-farouq <mediocre23534@gmail.com>
2025-02-13 14:23:49 +0200

    update loading component to be in ar and en

--- frontend/src/app/[locale]/loading.tsx
+++ frontend/src/app/[locale]/loading.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useTranslations } from "next-intl";
 
-const loading = () => {
+const Loading = () => {
+  const t = useTranslations("Loading");
+
   return (
     <div className="flex items-center justify-center bg-gray-100 absolute top-0 right-0 bottom-0 left-0">
       <div className="flex flex-col items-center gap-4">
         <div className="h-12 w-12 animate-spin rounded-full border-4 border-primary border-t-transparent shadow-lg"></div>
-        <p className="text-lg font-medium text-default-black">Loading...</p>
+        <p className="text-lg font-medium text-default-black">
+          {t("loading")}...
+        </p>
       </div>
     </div>
   );
 };
 
-export default loading;
+export default Loading;
