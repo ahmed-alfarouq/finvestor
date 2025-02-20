801c8fc5a7d1f3bc5b9e27a487ea489177e5e375
ahmed-al-farouq <mediocre23534@gmail.com>
2025-02-20 14:15:27 +0200

    update error page UI

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,8 +4,15 @@ import React from "react";
 const ErrorBoundary = ({ error }: { error: Error }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="text-2xl font-bold mb-4">Something went wrong!</div>
-      <p className="text-center">{error.message}</p>
+      <div className="text-destructive text-5xl font-bold mb-6">
+        Something went wrong!
+      </div>
+      <p className="text-center text-xl min-h-14">
+        <span className="block mb-2">
+          <strong>Error Name:</strong> {error.name}
+        </span>
+        <strong>Description:</strong> {error.message}
+      </p>
     </div>
   );
 };
