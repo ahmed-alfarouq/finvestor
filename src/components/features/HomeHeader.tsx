6f3cd1d7b3a223d637e513a38fdd7d0978958fcb
ahmed-al-farouq <mediocre23534@gmail.com>
2025-03-15 11:31:05 +0200

    use icon on mobile screens instead of regirster button

--- src/components/features/HomeHeader.tsx
+++ src/components/features/HomeHeader.tsx
@@ -2,9 +2,14 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { useIsMobile } from "@/hooks/use-mobile";
+
 import DarkModeToggle from "../dark-mode-toggle";
 
+import { LogInIcon } from "lucide-react";
+
 const HomeHeader = () => {
+  const isMobile = useIsMobile();
   return (
     <header className="flex items-end justify-between px-4 sm:px-8 py-4">
       <Image
@@ -14,12 +19,19 @@ const HomeHeader = () => {
         height={80}
       />
       <div className="flex items-center gap-4">
-        <Link
-          href="/register"
-          className="capitalize bg-primary dark:bg-primary-dark text-white dark:text-white text-lg px-7 py-2 rounded transition-all"
-        >
-          Register
-        </Link>
+        {isMobile ? (
+          <Link href="/register">
+            <LogInIcon />
+          </Link>
+        ) : (
+          <Link
+            href="/register"
+            className="capitalize bg-primary dark:bg-primary-dark text-white dark:text-white text-lg px-7 py-2 rounded transition-all"
+          >
+            Register
+          </Link>
+        )}
+
         <DarkModeToggle />
       </div>
     </header>
