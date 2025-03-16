1f0ba4c48a996ea8e4fe9e80078f93009f53e5d4
ahmed-al-farouq <mediocre23534@gmail.com>
2025-03-16 05:42:22 +0200

    extend user data and pass role and id to session

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,8 +1,10 @@
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 import bcrypt from "bcryptjs";
+
 import { LoginSchema } from "./schemas/auth";
-import { getUserByEmail } from "./lib/getUserFromDb";
+
+import { getUserByEmail, getUserById } from "./lib/getUserFromDb";
 
 import type { NextAuthConfig } from "next-auth";
 
@@ -23,7 +25,7 @@ export default {
             const user = await getUserByEmail(email);
 
             if (!user || !user.password) return null;
-            console.log(email, password);
+
             const passwordsMatch = await bcrypt.compare(
               password,
               user.password
@@ -38,4 +40,24 @@ export default {
       },
     }),
   ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      return token;
+    },
+  },
 } satisfies NextAuthConfig;
