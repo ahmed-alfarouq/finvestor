6f5982ecc3d95daeb5f69ffcdab5582f2e70d590
ahmed-al-farouq <mediocre23534@gmail.com>
2025-03-16 10:53:15 +0200

    add login button and update mobile screen UI

--- src/components/features/HomeHeader.tsx
+++ src/components/features/HomeHeader.tsx
@@ -2,16 +2,12 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useIsMobile } from "@/hooks/use-mobile";
 
 import DarkModeToggle from "../dark-mode-toggle";
 
-import { LogInIcon } from "lucide-react";
-
 const HomeHeader = () => {
-  const isMobile = useIsMobile();
   return (
-    <header className="flex items-end justify-between px-4 sm:px-8 py-4">
+    <header className="flex items-end justify-center sm:justify-between flex-wrap sm:flex-nowrap gap-10 px-4 sm:px-8 py-4">
       <Image
         src="/img/logo.webp"
         alt="Finvestor logo - Smart Investment App"
@@ -19,18 +15,18 @@ const HomeHeader = () => {
         height={80}
       />
       <div className="flex items-center gap-4">
-        {isMobile ? (
-          <Link href="/register">
-            <LogInIcon />
-          </Link>
-        ) : (
-          <Link
-            href="/register"
-            className="capitalize bg-primary dark:bg-primary-dark text-white dark:text-white text-lg px-7 py-2 rounded transition-all"
-          >
-            Register
-          </Link>
-        )}
+        <Link
+          href="/register"
+          className="capitalize bg-primary dark:bg-primary-dark text-white dark:text-white text-lg px-7 py-2 rounded transition-all"
+        >
+          Register
+        </Link>
+        <Link
+          href="/login"
+          className="capitalize bg-transparent text-default-black border border-default-black dark:border-white dark:text-white text-lg px-7 py-2 rounded transition-all"
+        >
+          Login
+        </Link>
 
         <DarkModeToggle />
       </div>
