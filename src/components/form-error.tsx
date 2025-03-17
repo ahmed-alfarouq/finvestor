de7c5193c83282bacae9cd92b67e53d65da9dfb0
ahmed-al-farouq <mediocre23534@gmail.com>
2025-03-17 08:36:16 +0200

    prevent icons from shrinking

--- src/components/form-error.tsx
+++ src/components/form-error.tsx
@@ -9,7 +9,7 @@ const FormError = ({ message }: FormErrorProps) => {
   if (!message) return null;
   return (
     <div className="bg-destructive/15 p-3 flex items-center gap-x-2 text-sm text-destructive dark:text-destructive-dark">
-      <AiFillExclamationCircle size={16} />
+      <AiFillExclamationCircle size={16} className="flex-shrink-0" />
       <p>{message}</p>
     </div>
   );
--- src/components/form-success.tsx
+++ src/components/form-success.tsx
@@ -9,7 +9,7 @@ const FormSuccess = ({ message }: FormErrorProps) => {
   if (!message) return null;
   return (
     <div className="bg-emerald-500/15 p-3 flex items-center gap-x-2 text-sm text-emerald-500">
-      <CheckCircle2Icon size={16} />
+      <CheckCircle2Icon size={16} className="flex-shrink-0" />
       <p>{message}</p>
     </div>
   );
