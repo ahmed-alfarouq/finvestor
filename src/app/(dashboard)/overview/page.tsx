0a99bf6f81e5f579f6e05b5764e5f243346d27be
ahmed-al-farouq <mediocre23534@gmail.com>
2025-03-20 07:40:38 +0200

    added total balance box

--- src/app/(dashboard)/overview/page.tsx
+++ src/app/(dashboard)/overview/page.tsx
@@ -1,3 +1,15 @@
+import TotalBalanceBox from "@/components/features/overview/TotalBalanceBox";
+
 export default function OverviewPage() {
-  return <main>Overview</main>;
+  return (
+    <section className="flex w-full flex-row max-xl:max-h-screen max-xl:overflow-y-scroll">
+      <section className="flex w-full flex-1 flex-col gap-8 px-5 sm:px-8 py-7 lg:py-12 xl:max-h-screen xl:overflow-y-scroll">
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={4}
+          totalCurrentBalance={1200}
+        />
+      </section>
+    </section>
+  );
 }
---
+++ src/components/features/overview/TotalBalanceBox.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import AnimatedCounter from "@/components/animated-counter";
+import DoughnutChart from "@/components/doughnut-chart";
+
+const TotalBalanceBox = ({
+  accounts = [],
+  totalBanks,
+  totalCurrentBalance,
+}: TotlaBalanceBoxProps) => {
+  return (
+    <section className="total-balance">
+      <div className="total-balance-chart">
+        <DoughnutChart accounts={accounts} />
+      </div>
+      <div className="flex flex-col gap-6">
+        <h2 className="text-lg font-semibold text-default-black dark:text-white">
+          Bank Accounts: {totalBanks}
+        </h2>
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">Total Current Balance</p>
+          <div className="total-balance-amount flex-center gap-2">
+            <AnimatedCounter amount={totalCurrentBalance} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TotalBalanceBox;
