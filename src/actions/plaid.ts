9dd94995c62d1bd2df1d5beb0eb087393f7156c5
ahmed-alfarouq <mediocre23534@gmail.com>
2025-04-09 11:06:56 +0200

    integrate plaid

---
+++ src/actions/plaid.ts
@@ -0,0 +1,118 @@
+"user server";
+import { v4 as uuidv4 } from "uuid";
+import { plaidClient } from "@/lib/plaid";
+import { encryptId, parseStringify } from "@/lib/utils";
+import { revalidatePath } from "next/cache";
+import {
+  CountryCode,
+  ProcessorTokenCreateRequest,
+  ProcessorTokenCreateRequestProcessorEnum,
+  Products,
+} from "plaid";
+import { addFundingSource } from "./dwolla";
+import { prisma } from "@/prisma";
+import {
+  createBankAccountProps,
+  exchangePublicTokenProps,
+  User,
+} from "@/types";
+
+export const createLinkToken = async (user: User) => {
+  try {
+    const tokenParams = {
+      user: {
+        client_user_id: user.id,
+      },
+      client_name: `${user.firstName} ${user.lastName}`,
+      products: ["auth"] as Products[],
+      language: "en",
+      country_codes: ["US", "EG", "CA"] as CountryCode[],
+    };
+
+    const res = await plaidClient.linkTokenCreate(tokenParams);
+    return parseStringify({ linkToken: res.data.link_token });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const createBankAccount = async ({
+  userId,
+  bankId,
+  accountId,
+  accessToken,
+  fundingSourceUrl,
+  sharableId,
+}: createBankAccountProps) => {
+  try {
+    await prisma.bankAccount.create({
+      data: {
+        id: uuidv4(),
+        userId,
+        bankId,
+        accountId,
+        accessToken,
+        fundingSourceUrl,
+        sharableId,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const exchangePublicToken = async ({
+  publicToken,
+  user,
+}: exchangePublicTokenProps) => {
+  try {
+    const res = await plaidClient.itemPublicTokenExchange({
+      public_token: publicToken,
+    });
+
+    const accessToken = res.data.access_token;
+    const itemId = res.data.item_id;
+
+    const accountsResponse = await plaidClient.accountsGet({
+      access_token: accessToken,
+    });
+
+    const accountData = accountsResponse.data.accounts[0];
+
+    const request: ProcessorTokenCreateRequest = {
+      access_token: accessToken,
+      account_id: accountData.account_id,
+      processor: "dwolla" as ProcessorTokenCreateRequestProcessorEnum,
+    };
+
+    const processorTokenResponse = await plaidClient.processorTokenCreate(
+      request
+    );
+    const processorToken = processorTokenResponse.data.processor_token;
+
+    const fundingSourceUrl = await addFundingSource({
+      dwollaCustomerId: user.dwollaCustomerId,
+      processorToken,
+      bankName: accountData.name,
+    });
+
+    if (!fundingSourceUrl) throw Error;
+
+    await createBankAccount({
+      userId: user.id,
+      bankId: itemId,
+      accountId: accountData.account_id,
+      accessToken,
+      fundingSourceUrl,
+      sharableId: encryptId(accountData.account_id),
+    });
+
+    revalidatePath("/");
+
+    return parseStringify({
+      publicTokenExchange: "complete",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
---
+++ src/components/ui/plaid-link.tsx
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from "react";
+import { PlaidLinkOptions, usePlaidLink } from "react-plaid-link";
+import { useRouter } from "next/navigation";
+
+import { createLinkToken, exchangePublicToken } from "@/actions/plaid";
+
+import { Button } from "./button";
+
+const plaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const router = useRouter();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const getLinkToken = async () => {
+      const data = await createLinkToken(user);
+      setToken(data?.token);
+    };
+    getLinkToken();
+  }, []);
+
+  const onSuccess = useCallback(
+    async (public_token: string) => {
+      await exchangePublicToken({ publicToken: public_token, user });
+      router.push("/");
+    },
+    [user]
+  );
+
+  const config: PlaidLinkOptions = {
+    token,
+    onSuccess,
+  };
+
+  const { open, ready } = usePlaidLink(config);
+  return (
+    <Button variant={variant} onClick={() => open()} disabled={!ready}>
+      Connect Bank
+    </Button>
+  );
+};
+
+export default plaidLink;
