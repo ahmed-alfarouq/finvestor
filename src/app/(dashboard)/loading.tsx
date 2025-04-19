115ac3590edade074c4f6bbe5bd1fa1869d76c09
ahmed-alfarouq <mediocre23534@gmail.com>
2025-04-19 06:49:46 +0200

    Refactor loading components to use a centralized PageLoader component and update styles in globals.css

--- src/app/(dashboard)/loading.tsx
+++ src/app/(dashboard)/loading.tsx
@@ -1,14 +1,7 @@
-import React from "react";
+import PageLoader from "@/components/page-loader";
 
 const Loading = () => {
-  return (
-    <div className="flex items-center justify-center bg-gray-100 dark:bg-[#1e293b] absolute top-0 right-0 bottom-0 left-0">
-      <div className="flex flex-col items-center gap-4">
-        <div className="h-12 w-12 animate-spin rounded-full border-4 border-primary dark:border-primary-dark dark:border-t-transparent border-t-transparent shadow-lg"></div>
-        <p className="text-lg font-medium text-default-black dark:text-white">Loading...</p>
-      </div>
-    </div>
-  );
+  return <PageLoader />;
 };
 
 export default Loading;
