01c97b953d4e94db920ffbdbafc32b810aacdece
ahmed-alfarouq <mediocre23534@gmail.com>
2025-04-19 07:23:46 +0200

    Refactor to enhance layout and stick to figma design

--- src/components/features/DashboardHeader.tsx
+++ src/components/features/DashboardHeader.tsx
@@ -7,21 +7,37 @@ import { SidebarSeparator, SidebarTrigger } from "@/components/ui/sidebar";
 import DarkModeToggle from "@/components/dark-mode-toggle";
 
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
-import { formatDateTime } from "@/lib/utils";
+import { cn, formatDateTime } from "@/lib/utils";
 
 const DashboardHeader = () => {
   const user = useCurrentUser();
   const isMobile = useIsMobile();
   return (
-    <header className="flex items-center justify-between shrink-0 h-16 border-b border-gray-4 dark:border-gray-5 px-4">
-      <div className="flex items-center sm:gap-2 px-1  text-default-black dark:text-white">
+    <header
+      className={cn(
+        "flex items-center justify-between shrink-0 h-20 border-b border-gray-4 dark:border-gray-5",
+        isMobile ? "container mx-auto" : "px-8"
+      )}
+    >
+      <div className="flex items-center sm:gap-2 px-1 text-default-black dark:text-white">
         <SidebarTrigger className="-ml-1" />
         <SidebarSeparator orientation="vertical" />
+
+        <h2 className="text-lg sm:text-2xl font-semibold capitalize">
+          hello, {user?.firstName}
+        </h2>
+
         {!isMobile && (
-          <h2 className="font-semibold capitalize">hello, {user?.firstName}</h2>
+          <>
+            <MdKeyboardDoubleArrowRight
+              size={24}
+              className="sm:ml-3 text-gray-2 dark:text-gray-4"
+            />
+            <span className="text-sm text-gray-2 dark:text-gray-4">
+              {formatDateTime(new Date()).dateOnly}
+            </span>
+          </>
         )}
-        <MdKeyboardDoubleArrowRight size={24} />
-        {formatDateTime(new Date()).dateOnly}
       </div>
       <DarkModeToggle />
     </header>
