76462174f896fdf2aaeeef772245a25f51ed55ea
ahmed-alfarouq <mediocre23534@gmail.com>
2025-04-22 07:17:43 +0200

    adjust column span to be applied only on large screens ">=1024px"

--- src/app/(dashboard)/overview/page.tsx
+++ src/app/(dashboard)/overview/page.tsx
@@ -41,7 +41,7 @@ const OverviewPage = async () => {
       </section>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
         <RecentTransactions transactions={transactions} />
-        <div className="col-span-2"></div>
+        <div className="lg:col-span-2"></div>
       </section>
     </section>
   );
