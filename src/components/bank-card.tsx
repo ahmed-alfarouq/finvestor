340aa34b4ed5ac1af8271140a549a06330b3091b
ahmed-alfarouq <mediocre23534@gmail.com>
2025-04-22 07:13:49 +0200

    XS "<=375px" screen: adjust padding, update layout for better responsiveness, and enhance text styles

--- src/components/bank-card.tsx
+++ src/components/bank-card.tsx
@@ -33,24 +33,23 @@ const BankCard = ({
     >
       <div className="absolute inset-0 bg-gradient-to-br from-primary to-primary-dark" />
 
-      <div className="relative p-6 h-full flex justify-between text-white">
-        <div className="flex flex-col justify-between items-start">
+      <div className="relative p-2 xs:p-6 h-full flex justify-between items-center text-white">
+        <div className="flex flex-col xs:justify-between items-start gap-1">
           <h3 className="text-sm font-medium">Bank Name</h3>
           <div className="text-lg font-medium">{bankName}</div>
-          <div className="text-sm font-mono text-gray-6">
+          <div className="text-sm font-medium text-gray-6">
             {`**** **** **** ${accountNumber}`}
           </div>
         </div>
 
-        <div className="flex flex-col justify-between items-center">
-          <div className="w-14 h-10 relative">
-            <Image
-              src={getCardTypeIcon()}
-              alt={type}
-              fill
-            />
+        <div className="flex flex-col justify-between items-center gap-[22px]">
+          <div className="w-10 h-7 xs:w-14 xs:h-10 relative">
+            <Image src={getCardTypeIcon()} alt={type} fill />
           </div>
-          <AnimatedCounter amount={balance} className="text-xl sm:text-2xl font-bold" />
+          <AnimatedCounter
+            amount={balance}
+            className="text-lg xs:text-xl sm:text-2xl font-bold"
+          />
         </div>
       </div>
     </div>
