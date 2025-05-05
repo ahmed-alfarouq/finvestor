2d2720613e2782062c5384041dcfca6ebb5609bf
ahmed-al-farouq <mediocre23534@gmail.com>
2025-05-05 09:47:24 +0300

    Refactor: Update user type in next-auth module to use a centralized User type.

--- next-ath.d.ts
+++ next-ath.d.ts
@@ -1,25 +1,10 @@
 import type { DefaultSession } from "next-auth";
-import type { BankAccountProps } from "@/types/index";
+import type { BankAccountProps, User } from "@/types/index";
 import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      id: string;
-      email: string;
-      role: string;
-      firstName: string;
-      lastName: string;
-      address1: string;
-      city: string;
-      state: string;
-      postalCode: string;
-      dateOfBirth: string;
-      ssn: string;
-      bankAccounts: BankAccountProps[];
-      dwollaCustomerUrl: string;
-      dwollaCustomerId: string;
-    } & DefaultSession["user"];
+    user: User & DefaultSession["user"];
   }
 }
 
