9b04d6b02cbb259ac6317ff498845acd8aea574b
ahmed-al-farouq <mediocre23534@gmail.com>
2025-05-05 09:48:45 +0300

    Feature: Add ModalWrapper and TargetForm components for savings goal management.

---
+++ src/components/modal-wrapper.tsx
@@ -0,0 +1,37 @@
+import { X } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const ModalWrapper = ({
+  children,
+  close,
+  isOpen,
+  className,
+}: {
+  children: React.ReactNode;
+  close: () => void;
+  isOpen: boolean;
+  className?: string;
+}) => {
+  return (
+    <section
+      role="dialog"
+      className={cn(
+        "fixed inset-0 z-50 flex items-center justify-center bg-default-black/60 transition-opacity duration-300",
+        isOpen ? "opacity-100" : "opacity-0 pointer-events-none",
+        className
+      )}
+    >
+      <div className="relative w-full sm:max-w-lg rounded-lg bg-default dark:bg-default-dark px-8 py-12 sm:p-16 sm:pb-12 mx-5 sm:mx-0">
+        <button
+          className="absolute top-4 right-5 text-secondary-color dark:text-secondary-color-dark"
+          onClick={close}
+        >
+          <X />
+        </button>
+        {children}
+      </div>
+    </section>
+  );
+};
+
+export default ModalWrapper;
---
+++ src/components/target-form.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { useState, useTransition } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { targetSchema, TargetSchema } from "@/schemas/target";
+
+import Timer from "@/components/timer";
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import FormError from "@/components/form-error";
+import FormInput from "@/components/auth/FormInput";
+import FormSuccess from "@/components/form-success";
+
+import { logout } from "@/actions/auth/logout";
+import { updateSavingsGoal } from "@/actions/user/updateUser";
+
+import useCurrentUser from "@/hooks/use-current-user";
+import { UseUpdateSessionContext } from "@/context/UpdateSessionContext";
+
+const TargetForm = ({
+  category,
+  onSuccess = () => {},
+}: {
+  category?: string;
+  onSuccess?: () => void;
+}) => {
+  const user = useCurrentUser();
+  const { update } = UseUpdateSessionContext();
+  const [pending, startTransition] = useTransition();
+
+  const [formError, setFormError] = useState<string | undefined>("");
+  const [formSuccess, setFormSuccess] = useState<string | undefined>("");
+  const [sessionError, setSessionError] = useState<boolean>(false);
+
+  const form = useForm<TargetSchema>({
+    resolver: zodResolver(targetSchema),
+    defaultValues: {
+      target: "",
+    },
+  });
+
+  const handleLogout = async () => await logout();
+
+  const onSubmit = (data: TargetSchema) => {
+    setFormError("");
+    setFormSuccess("");
+    startTransition(async () => {
+      if (!user) {
+        setFormError("Something went wrong with your session!");
+        setSessionError(true);
+        return;
+      }
+
+      let res;
+      if (category) {
+        /**
+         * To Do:
+         * Handle updating expense's target by category
+         */
+        return;
+      } else {
+        res = await updateSavingsGoal(user.id, data.target);
+      }
+
+      if (res.error) {
+        setFormError(res.error);
+        setSessionError(true);
+        return;
+      }
+      // Update session
+      const updatedSession = await update();
+
+      if (!updatedSession) {
+        setFormError("Something went wrong, please refresh the page!");
+        return;
+      }
+
+      setFormSuccess(res.success);
+      form.reset();
+      onSuccess();
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormInput
+          control={form.control}
+          type="number"
+          name="target"
+          label="Target Amount"
+          placeholder="5000000"
+          disabled={pending}
+        />
+        <FormError message={formError} />
+        <FormSuccess message={formSuccess} />
+        {sessionError && (
+          <div className="bg-destructive/15 p-3 flex items-center gap-x-2 text-sm text-destructive dark:text-destructive-dark">
+            <p>
+              You will be loged out after{" "}
+              <Timer duration={2} onComplete={handleLogout} />
+            </p>
+          </div>
+        )}
+        <Button
+          type="submit"
+          size="lg"
+          className="w-40 h-10 sm:w-48 sm:h-12 block mx-auto"
+          disabled={pending}
+        >
+          Save
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default TargetForm;
