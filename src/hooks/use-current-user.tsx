405d09a05082127e90f38862b80850f4c0046059
ahmed-al-farouq <mediocre23534@gmail.com>
2025-10-20 13:31:02 +0300

    refactor: Enhance useCurrentUser hook to utilize react-query for user data retrieval, improving session handling

--- src/hooks/use-current-user.tsx
+++ src/hooks/use-current-user.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { useSession } from "next-auth/react";
+import { useQuery } from "@tanstack/react-query";
+
+import { getUserById } from "@/actions/user/getUserFromDb";
 
 const useCurrentUser = () => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
+  const user = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => await getUserById(session!.user!.id!),
+    enabled: !!session && !!session.user,
+  });
 
-  return { user: session?.user, loading: status === "loading" };
+  return user.data;
 };
 
 export default useCurrentUser;
