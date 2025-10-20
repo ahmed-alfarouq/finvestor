1cc7b182143b0d0dd8dfc19ac7536901588a40c4
ahmed-al-farouq <mediocre23534@gmail.com>
2025-10-20 13:46:13 +0300

    refactor: Update Goals and ExpensesGoals components to utilize async data fetching and improve session handling, enhancing user experience and performance

--- src/app/(dashboard)/goals/page.tsx
+++ src/app/(dashboard)/goals/page.tsx
@@ -1,6 +1,4 @@
-"use client";
-import { useMemo } from "react";
-import useCurrentUser from "@/hooks/use-current-user";
+import { auth } from "@/auth";
 
 import PageContainer from "@/components/page-container";
 import GoalsBox from "@/components/features/overview/GoalsBox";
@@ -8,53 +6,40 @@ import EmptyGoalsBox from "@/components/features/EmptyGoalsBox";
 import ExpensesGoals from "@/components/features/goals/ExpensesGoals";
 import SelectedAccounts from "@/components/features/goals/SelectedAccounts";
 
-import { useBanksDataContext } from "@/context/BanksDataContext";
+import { getCachedUser } from "@/lib/cache/user";
 
-const Goals = () => {
-  const { user } = useCurrentUser();
-  const { accounts } = useBanksDataContext();
+const Goals = async () => {
+  const session = await auth();
 
-  const savingsAchievedAmount = useMemo(
-    () =>
-      accounts.reduce((total, acc) => {
-        return user?.savingsGoalAccounts.includes(acc.id)
-          ? total + (acc.availableBalance ?? 0)
-          : total;
-      }, 0),
-    []
-  );
+  if (!session) return;
+  const user = await getCachedUser(session.user.id);
+
+  const hasGoalAccounts = !!user.savingsGoalAccounts.length;
 
   return (
     <PageContainer>
       <section className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-3">
-        {user && user.savingsGoal && savingsAchievedAmount ? (
-          <GoalsBox
-            title="Goals"
-            targetAmount={Number(user.savingsGoal)}
-            achievedAmount={savingsAchievedAmount}
-            thisMonthTarget={Number(user.savingsGoal)}
-            date={new Date()}
-            className="md:col-span-1"
-          />
-        ) : user && user.savingsGoal && !savingsAchievedAmount ? (
+        {user && user.savingsGoal && hasGoalAccounts ? (
+          <GoalsBox className="md:col-span-2 lg:col-span-1" />
+        ) : user && user.savingsGoal && !hasGoalAccounts ? (
           <EmptyGoalsBox
             title="Goals"
             message="Select an account to continue setting up your goal."
             date={new Date()}
             showButton={false}
             selectedAccounts={false}
-            className="md:col-span-1"
+            className="md:col-span-2 lg:col-span-1"
           />
         ) : (
           <EmptyGoalsBox
             title="Goals"
             date={new Date()}
-            className="md:col-span-1"
+            className="md:col-span-2 lg:col-span-1"
           />
         )}
-        <SelectedAccounts className="md:col-span-2" />
+        <SelectedAccounts className="lg:col-span-2" />
       </section>
-      <ExpensesGoals />
+      <ExpensesGoals expensesGoals={user.expensesGoals} />
     </PageContainer>
   );
 };
--- src/components/features/goals/ExpensesGoals.tsx
+++ src/components/features/goals/ExpensesGoals.tsx
@@ -1,19 +1,20 @@
-"use client";
-import useCurrentUser from "@/hooks/use-current-user";
-import { useBanksDataContext } from "@/context/BanksDataContext";
-
 import ExpenseGoal from "./components/expense-goal";
 
-import { splitTransactionsByCategory } from "@/lib/transactions";
 import NotAvailable from "@/components/not-available";
 
-const ExpensesGoals = () => {
-  const { user } = useCurrentUser();
-  const { transactions } = useBanksDataContext();
+import { splitTransactionsByCategory } from "@/lib/transactions";
 
-  const expensesGoals = user?.expensesGoals;
+import { Transaction, User } from "@/types";
+
+const ExpensesGoals = ({
+  expensesGoals,
+}: {
+  expensesGoals: User["expensesGoals"];
+}) => {
   const getCategoryGoal = (category: string) =>
     expensesGoals?.find((g) => g.category === category)?.amount;
+
+  const transactions: Transaction[] = [];
 
   const transactionsByCategories = splitTransactionsByCategory([
     ...transactions,
@@ -36,10 +37,7 @@ const ExpensesGoals = () => {
             />
           ))
         ) : (
-          <NotAvailable
-            title=""
-            message="Oops! No transactions to show. Try connecting a checking or savings account."
-          />
+          <NotAvailable message="Oops! No transactions to show. Try connecting a checking or savings account." />
         )}
       </div>
     </section>
