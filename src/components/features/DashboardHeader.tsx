f75e58a4d2f7d433c82ec34349c454d9ea89872f
ahmed-al-farouq <mediocre23534@gmail.com>
2025-10-20 13:30:24 +0300

    refactor: Update DashboardHeader component to be server component, accept name prop, simplifying user greeting and improving mobile responsiveness

--- src/components/features/DashboardHeader.tsx
+++ src/components/features/DashboardHeader.tsx
@@ -1,43 +1,27 @@
-"use client";
-import React from "react";
-import useCurrentUser from "@/hooks/use-current-user";
-import { useIsMobile } from "@/hooks/use-mobile";
-
 import { SidebarSeparator, SidebarTrigger } from "@/components/ui/sidebar";
 import DarkModeToggle from "@/components/dark-mode-toggle";
 
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
-import { cn, formatDateTime } from "@/lib/utils";
+import { formatDateTime } from "@/lib/utils";
 
-const DashboardHeader = () => {
-  const { user } = useCurrentUser();
-  const isMobile = useIsMobile();
+const DashboardHeader = ({ name }: { name: string }) => {
   return (
-    <header
-      className={cn(
-        "flex items-center justify-between shrink-0 h-20 border-b border-gray-4 dark:border-gray-5",
-        isMobile ? "container mx-auto" : "px-8"
-      )}
-    >
+    <header className="w-full px-4 md:px-8 flex items-center justify-between shrink-0 h-20 border-b border-gray-4 dark:border-gray-5">
       <div className="flex items-center sm:gap-2 px-1 text-default-black dark:text-white">
         <SidebarTrigger className="-ml-1" />
         <SidebarSeparator orientation="vertical" />
 
         <h2 className="text-lg sm:text-2xl font-semibold capitalize">
-          hello, {user?.firstName}
+          hello, {name}
         </h2>
 
-        {!isMobile && (
-          <>
-            <MdKeyboardDoubleArrowRight
-              size={24}
-              className="sm:ml-3 text-gray-2 dark:text-gray-4"
-            />
-            <span className="text-sm text-gray-2 dark:text-gray-4">
-              {formatDateTime(new Date()).dateOnly}
-            </span>
-          </>
-        )}
+        <MdKeyboardDoubleArrowRight
+          size={24}
+          className="hidden md:block sm:ml-3 text-gray-2 dark:text-gray-4"
+        />
+        <span className="hidden md:block text-sm text-gray-2 dark:text-gray-4">
+          {formatDateTime(new Date()).dateOnly}
+        </span>
       </div>
       <DarkModeToggle />
     </header>
