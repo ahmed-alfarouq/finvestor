0755845e717b8e9e44ba20609d47a760ea7784d3
ahmed-al-farouq <mediocre23534@gmail.com>
2025-10-20 13:27:32 +0300

    feat: Enhance DarkModeToggle component with client-side rendering and improved icon display

--- src/components/dark-mode-toggle.tsx
+++ src/components/dark-mode-toggle.tsx
@@ -1,18 +1,27 @@
 "use client";
-
-import React from "react";
 import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const DarkModeToggle = () => {
+  const [isClient, setIsClient] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => setIsClient(true), []);
+
+  if (!isClient) return;
 
   return (
     <button
       type="button"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="bg-default-black dark:bg-white shadow-md dark:shadow-cyan-500/50 py-1 px-2 rounded-full transition"
+      className="bg-default-black dark:bg-white shadow-md dark:shadow-cyan-500/50 py-1 px-2.5 rounded-full transition"
     >
-      {theme === "dark" ? "🌞" : "🌙"}
+      {theme === "dark" ? (
+        <Sun className="text-yellow-600" />
+      ) : (
+        <Moon className="text-white" />
+      )}
     </button>
   );
 };
