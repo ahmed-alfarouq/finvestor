4b6f6c7d04375b2f8f9e87f8f2c58ed05b8bd203
ahmed-al-farouq <mediocre23534@gmail.com>
2025-10-21 17:08:55 +0300

    feat: Add transaction retrieval functions for syncing and categorizing account transactions

---
+++ src/actions/transactions.ts
@@ -0,0 +1,89 @@
+"use server";
+import { plaidClient } from "@/plaid";
+
+import { handleError } from "@/lib/errors/handleError";
+
+import { Transaction } from "plaid";
+import { AxiosResponse } from "axios";
+import { TransactionsSyncResponse } from "plaid";
+
+export const getAccountTransactions = async (
+  accessToken: string,
+  accountId: string
+) => {
+  const response: AxiosResponse<TransactionsSyncResponse> =
+    await plaidClient.transactionsSync({
+      access_token: accessToken,
+      options: {
+        account_id: accountId,
+      },
+    });
+
+  return response.data.added;
+};
+
+export const getTransactions = async (accessToken: string, cursor?: string) => {
+  try {
+    if (accessToken === "")
+      return {
+        transactions: [],
+        nextCursor: undefined,
+        hasMore: false,
+      };
+
+    const response: AxiosResponse<TransactionsSyncResponse> =
+      await plaidClient.transactionsSync({
+        access_token: accessToken,
+        cursor,
+        count: 10,
+      });
+
+    return {
+      transactions: response.data.added,
+      nextCursor: response.data.next_cursor,
+      hasMore: response.data.has_more,
+    };
+  } catch (err) {
+    handleError(
+      err,
+      "An unexpected error occurred while getting the transactions."
+    );
+  }
+};
+
+export const getRecentTransactions = async (
+  accessToken: string,
+  count: number
+): Promise<
+  | {
+      all: Transaction[];
+      revenue: Transaction[];
+      expenses: Transaction[];
+    }
+  | undefined
+> => {
+  const transactionsRes: AxiosResponse<TransactionsSyncResponse> =
+    await plaidClient.transactionsSync({
+      access_token: accessToken,
+      count,
+    });
+
+  if (!transactionsRes.data.added.length) return;
+
+  const transactions = transactionsRes.data.added;
+
+  const all = [];
+  const revenue = [];
+  const expenses = [];
+
+  for (const transaction of transactions) {
+    all.push(transaction);
+    if (transaction.amount < 0) {
+      revenue.push(transaction);
+      continue;
+    }
+    expenses.push(transaction);
+  }
+
+  return { all, revenue, expenses };
+};
