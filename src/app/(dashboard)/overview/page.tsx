76a23fd2343a1189ec01092ad5fc55c88de7fafd
ahmed-al-farouq <mediocre23534@gmail.com>
2025-10-25 09:06:06 +0300

    refactor: Consolidate transaction data in OverviewPage by integrating dummy and last year's transactions for enhanced analytics

--- src/app/(dashboard)/overview/page.tsx
+++ src/app/(dashboard)/overview/page.tsx
@@ -9,6 +9,10 @@ import ExpensesBox from "@/components/features/overview/ExpensesBox";
 import StatisticsBox from "@/components/features/overview/StatisticsBox";
 import TotalBalanceBox from "@/components/features/overview/TotalBalanceBox";
 import RecentTransactions from "@/components/features/overview/RecentTransactions";
+import {
+  dummyTransactions,
+  lastYearTransactions,
+} from "@/constants/transactions";
 
 const OverviewPage = async () => {
   const session = await auth();
@@ -16,10 +20,16 @@ const OverviewPage = async () => {
   if (!session) return;
 
   const user = await getCachedUser(session.user.id);
-  const transactions = await getCachedTransactions(
+  const { transactions } = await getCachedTransactions(
     session.user.id,
     user.banks[0].accessToken
   );
+
+  const allTransactions = [
+    ...transactions,
+    ...dummyTransactions,
+    ...lastYearTransactions,
+  ];
 
   return (
     <PageContainer>
@@ -32,8 +42,8 @@ const OverviewPage = async () => {
         <RecentTransactions />
         {transactions && (
           <div className="h-full grid grid-cols-1 grid-rows-2 gap-8 md:gap-3 lg:col-span-2">
-            <StatisticsBox transactions={transactions.transactions} />
-            <ExpensesBox transactions={transactions.transactions} />
+            <StatisticsBox transactions={allTransactions} />
+            <ExpensesBox transactions={transactions} />
           </div>
         )}
       </section>
