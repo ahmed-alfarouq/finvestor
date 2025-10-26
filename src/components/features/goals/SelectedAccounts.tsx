9f0d299e289066c82e62660eff14d9d8d14f22cd
ahmed-al-farouq <mediocre23534@gmail.com>
2025-10-26 11:56:46 +0300

    refactor: updated SelectedAccounts to improve account filtering and messaging

--- src/components/features/goals/SelectedAccounts.tsx
+++ src/components/features/goals/SelectedAccounts.tsx
@@ -14,33 +14,32 @@ const SelectedAccounts = async ({ className }: { className?: string }) => {
   const user = await getCachedUser(session.user.id);
   const accounts = await getCachedAccounts(session.user.id);
 
-  const hasAccounts =
-    accounts &&
-    accounts?.filter(
-      (account) =>
-        account.subtype === "savings" || account.subtype === "checking"
-    );
+  const validAccounts = accounts.filter(
+    (account) => account.subtype === "savings" || account.subtype === "checking"
+  );
 
   return (
     <section className={cn("flex flex-col", className)}>
       <h2 className="card-title">Savings Accounts</h2>
 
       <div className="flex-1 flex flex-col gap-2 rounded-xl bg-default dark:bg-default-dark card-shadow p-4 sm:px-7 sm:py-5 overflow-hidden">
-        <h3 className="default-black text-base font-bold mb-1">
-          Your Linked Savings Accounts
-        </h3>
-        <p className="text-sm text-gray-2 dark:text-gray-7">
-          Select the accounts you want to include in your Target Achieved total.
-        </p>
-
-        {hasAccounts ? (
-          <SelectedAccountsForm
-            checkedAccounts={user?.savingsGoalAccounts || []}
-            userId={user?.id || ""}
-            accounts={accounts}
-          />
+        {validAccounts.length ? (
+          <>
+            <h3 className="default-black text-base font-bold mb-1">
+              Your Linked Savings Accounts
+            </h3>
+            <p className="text-sm text-gray-2 dark:text-gray-7">
+              Select the accounts you want to include in your Target Achieved
+              total and Savings Goals.
+            </p>
+            <SelectedAccountsForm
+              userId={user.id}
+              accounts={validAccounts}
+              checkedAccounts={user.savingsGoalAccounts}
+            />
+          </>
         ) : (
-          <div className="text-sm text-muted-foreground italic">
+          <div className="text-xl text-center text-muted-foreground italic">
             No savings/Checking accounts linked yet.
           </div>
         )}
--- src/components/features/goals/selectedAccounts/SelectedAccountsForm.tsx
+++ src/components/features/goals/selectedAccounts/SelectedAccountsForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useForm } from "react-hook-form";
-import { useSession } from "next-auth/react";
 import { useState, useTransition } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -26,18 +25,14 @@ const SelectedAccountsForm = ({
   checkedAccounts,
   accounts,
 }: SelectAccountFormProps) => {
-  const { update } = useSession();
-
   const [pending, startTransation] = useTransition();
   const [formError, setFormError] = useState<string | undefined>("");
   const [formSuccess, setFormSuccess] = useState<string | undefined>("");
 
-  const validAccounts = ["savings", "checking"];
-
   const form = useForm<SelectAccountsSchema>({
     resolver: zodResolver(selectAccountsSchema),
     defaultValues: {
-      selectedBankIds: [],
+      selectedAccountsIds: [],
     },
   });
 
@@ -52,53 +47,46 @@ const SelectedAccountsForm = ({
     setFormSuccess("");
 
     // Check if user didn't make any changes
-    const changesMade = arraysEqual(checkedAccounts, data.selectedBankIds);
+    const changesMade = arraysEqual(checkedAccounts, data.selectedAccountsIds);
     if (changesMade && !!checkedAccounts.length) {
       setFormError(
         "Please select at least one different savings goal account."
       );
       return;
     }
-
     startTransation(async () => {
-      const res = await updateSavingsGoalAccounts(userId, data.selectedBankIds);
+      const res = await updateSavingsGoalAccounts(
+        userId,
+        data.selectedAccountsIds
+      );
       if (res.error) {
         setFormError(res.error);
-        return;
-      }
-      // Update session
-      const updatedSession = await update();
-
-      if (!updatedSession) {
-        setFormError("Something went wrong, please refresh the page!");
         return;
       }
 
       setFormSuccess(res.success);
     });
   };
+
   return (
     <Form {...form}>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="h-[inherit] flex flex-col items-start"
+        className="max-h-96 h-[inherit] flex flex-col items-start"
       >
         <div className="grid gap-4 xl:grid-cols-2 w-full items-center my-3 max-h-[55%] sm:max-h-[8rem] overflow-y-auto">
-          {accounts.map(
-            (account) =>
-              validAccounts.includes(account.subtype) && (
-                <Checkbox
-                  key={account.id}
-                  register={register}
-                  registerName="selectedBankIds"
-                  label={`${account.name} (${account.institutionName})`}
-                  value={account.id}
-                  checked={checkedAccounts.includes(account.id)}
-                />
-              )
-          )}
+          {accounts.map((account) => (
+            <Checkbox
+              key={account.id}
+              register={register}
+              registerName="selectedAccountsIds"
+              label={`${account.name} (${account.institutionName})`}
+              value={account.id}
+              checked={checkedAccounts.includes(account.id)}
+            />
+          ))}
         </div>
-        <FormError message={errors.selectedBankIds?.message || formError} />
+        <FormError message={errors.selectedAccountsIds?.message || formError} />
         <FormSuccess message={formSuccess} />
 
         <Button
