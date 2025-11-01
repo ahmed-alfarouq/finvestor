af50016f612a4377293195ae0509bffd7350739b
ahmed-al-farouq <mediocre23534@gmail.com>
2025-11-01 18:11:21 +0200

    refactor: improve email sending functions by adding error handling and updating message validity durations

--- src/actions/mail.ts
+++ src/actions/mail.ts
@@ -1,6 +1,8 @@
 "use server";
 import sendEmail from "@/actions/sendEmail";
 
+import { handleError } from "@/lib/errors/handleError";
+
 export const sendVerificationEmail = async ({
   username,
   email,
@@ -12,7 +14,7 @@ export const sendVerificationEmail = async ({
 }) => {
   try {
     const html = `
-      <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); text-align: center;">
+      <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
       <h1 style="color: #299D91; font-size: 24px; font-weight: bold;">Welcome to Finvestor, ${username}!</h1>
       <p style="color: #4b5563; font-size: 16px; margin-top: 16px;">
           We're excited to have you on board! To get started, please verify your email by clicking the button below.
@@ -22,6 +24,8 @@ export const sendVerificationEmail = async ({
           Verify Email
       </a>
       <p style="color: #4b5563; font-size: 14px; margin-top: 20px;">
+          This link is valid for 5 minutes. 
+          <br />
           If you didn’t sign up for a Finvestor account, you can safely ignore this email.
       </p>
       <p style="color: #6b7280; font-size: 12px; margin-top: 20px;">
@@ -35,8 +39,11 @@ export const sendVerificationEmail = async ({
       subject: `${username}, Activate Your Finvestor Account Today`,
       html,
     });
-  } catch {
-    throw new Error("We couldn’t send the verification email. Please try again.");
+  } catch (err) {
+    handleError(
+      err,
+      "We couldn’t send the verification email. Please try again."
+    );
   }
 };
 
@@ -49,8 +56,9 @@ export const sendResetEmail = async ({
   email: string;
   token: string;
 }) => {
-  const html = `
-    <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); text-align: center;">
+  try {
+    const html = `
+    <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
     <h1 style="color: #299D91; font-size: 24px; font-weight: bold;">Hi ${username},</h1>
     <p style="color: #4b5563; font-size: 16px; margin-top: 16px;">
         We received a request to reset your password for your Finvestor account. If you made this request, please click the button below to set a new password:
@@ -59,65 +67,77 @@ export const sendResetEmail = async ({
         style="display: flex; gap: 10px; align-items: center; background-color: #299d91; color: #ffffff; margin: 0 auto; width: fit-content; padding: 12px 17px; border-radius: 3px; text-decoration: none; font-weight: bold;">
         🔒 Reset Password
     </a>
-    <p style="text-align:left; color: #4b5563; font-size: 14px; margin-top: 20px;">
-        This link is valid for 30 minutes. If you didn’t request a password reset, you can safely ignore this email—your account is secure.
+    <p style="color: #4b5563; font-size: 14px; margin-top: 20px;">
+        This link is valid for 5 minutes.
+        <br />
+        If you didn’t request a password reset, you can safely ignore this email—your account is secure.
     </p>
-    <p style="text-align:left; color: #6b7280; font-size: 12px; margin-top: 20px;">
+    <p style="color: #6b7280; font-size: 12px; margin-top: 20px;">
         If you need any assistance, feel free to reach out to our support team.
         <br />
         Best regards,
         <br />
         <strong>The Finvestor Team<strong>
     </p>
-    <div style="text-align:left;">
+    <div>
       🔗 <a href="${process.env.SITE_URL}" >Visit Finvestor</a>
     </div>
     </div>
   `;
-  await sendEmail({
-    to: email,
-    subject: `Reset Your Password – Action Required`,
-    html,
-  });
+    await sendEmail({
+      to: email,
+      subject: `Reset Your Password – Action Required`,
+      html,
+    });
+  } catch (err) {
+    handleError(err, "We couldn’t send the reset email. Please try again.");
+  }
 };
 
-export const sendTwoFactorTokenEmail = async ({
+export const sendTwoFactorCodeEmail = async ({
   username,
   email,
-  token,
+  code,
 }: {
   username: string;
   email: string;
-  token: string;
+  code: string;
 }) => {
-  const html = `
-      <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); text-align: center;">
+  try {
+    const html = `
+      <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
       <h1 style="color: #299D91; font-size: 24px; font-weight: bold;">Dear ${username},</h1>
       <p style="color: #4b5563; font-size: 16px; margin-top: 16px;">
           For added security, please use the following code to complete your login to Finvestor:
       </p>
-      <h2>Your 2FA Code: ${token}</h2>
+      <h2>Your 2FA Code: ${code}</h2>
       <p style="color: #4b5563; font-size: 14px; margin-top: 20px;">
-        This code is valid for 30 minutes. If you did not request this, please ignore this email or contact our support team immediately.
+        This code is valid for 5 minutes. If you did not request this, please ignore this email or contact our support team immediately.
       </p>
-      <p style="text-align:left; color: #4b5563; font-size: 14px; margin-top: 20px;">
+      <p style="color: #4b5563; font-size: 14px; margin-top: 20px;">
           For your security, never share this code with anyone.
       </p>
-      <p style="text-align:left; color: #6b7280; font-size: 12px; margin-top: 20px;">
+      <p style="color: #6b7280; font-size: 12px; margin-top: 20px;">
           If you need any assistance, feel free to reach out to our support team.
           <br />
           Stay secure,
           <br />
           <strong>The Finvestor Team<strong>
       </p>
-      <div style="text-align:left;">
+      <div>
         🔗 <a href="${process.env.SITE_URL}" >Visit Finvestor</a>
       </div>
       </div>
     `;
-  await sendEmail({
-    to: email,
-    subject: `Your Finvestor Two-Factor Authentication (2FA) Code`,
-    html,
-  });
+    await sendEmail({
+      to: email,
+      subject: `Your Finvestor Two-Factor Authentication (2FA) Code`,
+      html,
+    });
+  } catch (err) {
+    handleError(
+      err,
+      "We couldn’t send the two-factor code email. Please try again."
+    );
+  }
 };
